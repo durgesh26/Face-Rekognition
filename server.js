b736e62566b4bfac9c807f7c3ce2dcc0f05c0fc4
durgesh26 <73623270+durgesh26@users.noreply.github.com>
2021-05-07 12:19:07 -0400

    updated

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
+
 const express = require('express');
+const AWS = require('aws-sdk');
+const multer = require('multer');
 const app = express();
 const port = 8001;
-
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
 const cors = require('cors');
 const axios = require('axios').default;
+const uuid = require('uuid').v4;
 
+//code to initialize aws S3 bucket with the access key and secret key
+const s3 = new AWS.S3({
+	accessKeyId: "AKIAQ4QSFQVEFTNEHKTZ",
+	secretAccessKey: "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"
+})
+
+// code to store the file in memory storage using multer
+const storage = multer.memoryStorage({
+	destination: function(req,file,callback){
+		callback(null,'')
+	}
+});
+
+//code to upload single image file using multer
+const upload = multer({storage}).single('image');
+
+
+//code to initialize options for the Swagger UI
 const options = {
 	swaggerDefinition: {
 		info: {
@@ -22,6 +43,7 @@ const options = {
 
 const specs = swaggerJsdoc(options);
 
+//code to display Swagger UI on the endpoint /docs
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(specs));
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
@@ -29,7 +51,48 @@ app.use(express.json());
 
 
 
+//code to upload image to s3 bucket with the endpoint bucket
+app.post('/upload',upload, (req,res) => {
 
+	let myFile = req.file.originalname.split(".");
+	const fileType = myFile[myFile.length -1];
+
+	const params = {
+		Bucket: "facerekognition01",
+		Key: `${uuid()}.${fileType}`,
+		Body: req.file.buffer 
+	}
+	//code to upload image to s3 bucket
+	s3.upload(params, (err,data) => {
+		if(err){
+			res.status(500).send(err);
+		}else{
+			res.status(200).send(data);
+		}
+	})
+});
+
+//code to detect face from the image on the endpoint /detect
+app.get('/detect', (req,res) => {
+	console.log(req.file.originalname)
+	var params = {
+        Bucket: "facerekognition01",
+        MaxKeys: 1000
+    }
+
+	s3.listObjects(params, function(err,data) {
+		if(err){
+			res.status(500).send(err);
+		}else{
+			
+			res.status(200).send(data);
+		}
+	})
+
+});
+
+
+//code to listen the api at the predefined port
 app.listen(port, () => {
 	console.log('Example app listening at http://localhost:'+port);
 })
